b71a8aa139f3ade6c64c889bbd3e14540bb0d731
0041Bayu <115551745+0041Bayu@users.noreply.github.com>
2023-01-31 22:43:13 +0700

    Delete Penilaian Kinerja directory

--- Penilaian Kinerja/app/Views/Karyawan/index.php
+++
@@ -1,7 +0,0 @@
-<?= $this->extend('layout/pages_layout')?>
-
-<?= $this->section('content')?>
-
-<h1>Halaman Data Karyawan</h1>
-
-<?= $this->endSection()?>
--- Penilaian Kinerja/app/Views/home.php
+++
@@ -1,7 +0,0 @@
-<?= $this->extend('layout/pages_layout')?>
-
-<?= $this->section('content')?>
-
-<h1>Hello, world!</h1>
-
-<?= $this->endSection()?>
